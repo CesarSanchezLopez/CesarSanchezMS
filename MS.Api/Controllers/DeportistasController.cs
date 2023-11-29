1201f975c762697cade332112dfcf1adf359af95
Cesar A <sistemascesar@hotmail.com>
2023-11-29 17:39:33 -0500

    Ajuste

---
+++ MS.Api/Controllers/DeportistasController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MS.Core.Models;
+using MS.Core.Interfaces;
+using MS.Api.Interfaces;
+
+namespace MS.Api.Controllers
+{
+    public class DeportistasController : Controller, IDeportistasController
+    {
+        private IDeportistas _Repo;
+
+
+        public DeportistasController(IDeportistas pRepo)
+        {
+            _Repo = pRepo;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("Deportistas")]
+        [Route("api/Deportistas")]
+        public async Task<List<Deportista>> Deportistas()
+        {
+            return await _Repo.Deportistas();
+        }
+
+        [HttpGet("DeportistasModalidad")]
+        [Route("api/DeportistasModalidad")]
+        public async Task<List<Deportista>> DeportistasModalidad()
+        {
+            return await _Repo.DeportistasModalidad();
+        }
+    }
+}
---
+++ MS.Api/Interfaces/IDeportistasController.cs
@@ -0,0 +1,11 @@
+﻿using MS.Core.Models;
+
+namespace MS.Api.Interfaces
+{
+    public interface IDeportistasController
+    {
+
+        Task<List<Deportista>> Deportistas();
+        Task<List<Deportista>> DeportistasModalidad();
+    }
+}
